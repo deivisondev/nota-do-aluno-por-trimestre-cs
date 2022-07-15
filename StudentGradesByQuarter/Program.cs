af6ae41f99a2fdd56d9d71b5d00e894ad400645c
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-15 20:28:44 -0300

    Início

---
+++ StudentGradesByQuarter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace StudentGradesByQuarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.Write("Nome do aluno: ");
+            student.Name = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            student.FirstGrade = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.SecondGrade = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.ThirdGrade = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ StudentGradesByQuarter/Student.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace StudentGradesByQuarter
+{
+    public class Student
+    {
+        public string Name;
+        public float FirstGrade;
+        public float SecondGrade;
+        public float ThirdGrade;
+
+        public float FinalGrade()
+        {
+            return FirstGrade + SecondGrade + ThirdGrade;
+        }
+
+        public bool IsApproved()
+        {
+            if (FinalGrade() >= 60f) return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            if (IsApproved())
+            {
+                return $"NOTA FINAL = {FinalGrade().ToString("F2", CultureInfo.InvariantCulture)}\nAPROVADO";
+            }
+            else
+            {
+                return $"NOTA FINAL = {FinalGrade().ToString("F2", CultureInfo.InvariantCulture)}\nREPROVADO\nFALTAM {(60 - FinalGrade()).ToString("F2", CultureInfo.InvariantCulture)} PONTOS";
+            }
+        }
+    }
+}
